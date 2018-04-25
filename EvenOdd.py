d690785eeae6e952f52caa2138dd6e94b946fa3f
SPENCEINFO\smaripadaga <smaripadaga@spins.com>
2018-04-25 15:21:02 -0500

    EvenOdd script

---
+++ EvenOdd.py
@@ -0,0 +1,6 @@
+Number = int(input("Enter Number\n"))
+if Number%2 == 0:
+    print("This number " + str(Number) + " is Even")
+elif Number%2 == 1:
+    print("This number " + str(Number) + " is Odd")
+
