48c87e4df57a1d3ad60f3bceb45f0d38faf9cd9b
Shalini Maripadaga <mari.shalini@gmail.com>
2018-08-16 18:00:22 -0500

    Add files via upload

---
+++ comparison_operators.py
@@ -0,0 +1,14 @@
+#comparison operators
+x = int(10)
+if x < 15:
+    print ('x is less than 15')
+if x <= 10:
+    print('x is 10')
+if x == 10:
+    print ('x is actually 10')
+if x >= 5:
+    print('x is greater than or equal to 5')
+if x > 9:
+    print('x is greater than 9')
+if x != 9:
+    print(x)
